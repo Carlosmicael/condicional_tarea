b93dcdd9b5e6e55bdc546f766bce807dedcea09b
carlosmicael <carlosmlo381@gmail.com>
2022-11-15 22:50:08 -0800

    first commit

--- src/fecha.java
+++ src/fecha.java
@@ -1,16 +1,57 @@
+
+
 import java.util.Scanner;
 
 public class fecha {
     public static void main(String[] args) {
         System.out.println("programa para determinar una fecha");
         Scanner teclado = new Scanner(System.in);
-        System.out.println("ingresa tu fecha en formato aaaa/mm/aa");
-        int fecha = teclado.nextInt();
-        if (fecha <= 1990 && fecha >= 5.405913978494624){
-        System.out.println("corecto");
-        }else{
-            System.out.println("correcto");
+        System.out.println("ingresa una fecha");
+        System.out.println("Ingresa el año");
+        int year = teclado.nextInt();
+        System.out.println("Ingresa el mes");
+        int mes = teclado.nextInt();
+        System.out.println("Ingresa el día");
+        int dia = teclado.nextInt();
+        if (year >= 1990 && year <= 2010) {
+            if (mes == 1)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 2)
+                if (dia >= 1 && dia <= 28)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 3)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 4)
+                if (dia >= 1 && dia <= 30)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 5)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 6)
+                if (dia >= 1 && dia <= 30)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 7)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 8)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 9)
+                if (dia >= 1 && dia <= 30)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 10)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 11)
+                if (dia >= 1 && dia <= 30)
+                    System.out.println("La fecha ingresada es correcta");
+            if (mes == 12)
+                if (dia >= 1 && dia <= 31)
+                    System.out.println("La fecha ingresada es correcta");
+                else
+                    System.out.println("La fecha ingresada es incorrecta");
         }
     }
 }
-
